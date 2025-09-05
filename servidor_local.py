54d6572e66dbdb1ed66f9983fa2a81eae2ca6b04
oyupanqui27 <oyupanqui@pucp.pe>
2025-09-05 11:58:02 -0500

    Add files via upload

---
+++ servidor_local.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def recibir_datos():
+    datos = request.json
+    print("Datos recibidos:", datos)
+    # Aquí puedes guardar los datos o procesarlos
+    return jsonify({"status": "ok"}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
